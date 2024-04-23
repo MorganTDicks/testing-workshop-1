73c1cd41aa6c0967b5fa3a661b9aa5d7b7485aaf
MorganTDicks <morgan@al.co.za>
2024-04-23 12:44:47 +0200

    added boilerplate

---
+++ components/SimpleButton/__tests__/SimpleButton.test.tsx
@@ -0,0 +1,8 @@
+// Button.test.js
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Button from '@/components/SimpleButton/simple-button';
+
+describe("Writing simple tests", ()=>{
+   
+})
